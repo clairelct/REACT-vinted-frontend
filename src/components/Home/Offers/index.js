b09e687669e09f2a53732c7d1f6b66c5fdb8aa45
Claire Leconte <claire.lcnt@gmail.com>
2020-11-15 13:36:16 +0100

    Display nb. of offers with select button

--- src/components/Home/Offers/index.js
+++ src/components/Home/Offers/index.js
@@ -5,69 +5,34 @@ import DisplayUser from "../../Shared/DisplayUser";
 import axios from "axios";
 
 const Offers = ({ data }) => {
-  //console.log(data.count);
-
-  // // State Nb. d'offres par page
-  // // Par défaut le nb. total de produits
-  // const [nbOffersPage, setNbOffersPage] = useState(data.count);
-
-  // let limit;
-
-  // const handleNbOffers = (e) => {
-  //   const value = e.target.value;
-  //   setNbOffersPage(value);
-  //   console.log(nbOffersPage);
-  //   limit = value;
-  //   console.log("limit :", limit);
-  // };
-
   return (
-    <main>
-      <div className="container">
-        <div className="pagination">
-          <span>Nombre de produits par page :</span>
-          {/* <select
-            value={nbOffersPage}
-            onChange={handleNbOffers}
-            name="nbOffersPerPage"
-            id="nbOffersPerPage"
-          >
-            <option value="">--Please choose an option--</option>
-            <option value="3">3 produits par page</option>
-            <option value="6">6 produits par page</option>
-            <option value="9">9 produits par page</option>
-          </select> */}
-        </div>
-
-        <div className="offers-container">
-          {data.offers.map((offer, index) => {
-            return (
-              <div key={offer._id} className="card-container">
-                <Link to={`/offer/${offer._id}`}>
-                  <DisplayUser
-                    src={offer.owner.account.avatar}
-                    alt=""
-                    username={offer.owner.account.username}
-                  />
-                  <div className="display-productimg">
-                    {/* Modifier */}
-                    <img
-                      src={offer.product_image.picture.result.secure_url}
-                      alt={offer.product_image.picture.result.original_filename}
-                    />
-                  </div>
-                  <div className="display-productinfos">
-                    <span>{offer.product_price} €</span>
-                    <span>{offer.product_details[1].TAILLE}</span>
-                    <span>{offer.product_details[0].MARQUE}</span>
-                  </div>
-                </Link>
+    <div className="offers-container">
+      {data.offers.map((offer, index) => {
+        return (
+          <div key={offer._id} className="card-container">
+            <Link to={`/offer/${offer._id}`}>
+              <DisplayUser
+                src={offer.owner.account.avatar}
+                alt=""
+                username={offer.owner.account.username}
+              />
+              <div className="display-productimg">
+                {/* Modifier */}
+                <img
+                  src={offer.product_image.picture.result.secure_url}
+                  alt={offer.product_image.picture.result.original_filename}
+                />
               </div>
-            );
-          })}
-        </div>
-      </div>
-    </main>
+              <div className="display-productinfos">
+                <span>{offer.product_price} €</span>
+                <span>{offer.product_details[1].TAILLE}</span>
+                <span>{offer.product_details[0].MARQUE}</span>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -10,11 +10,18 @@ const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
   // State affichage des boutons de page, tab. car mapper dessus pour afficher x boutons
   const [pages, setPages] = useState([]);
+  // Au clic sur une page
+  const [page, setPage] = useState(1);
+
+  // State Nb. d'offres par page (par défaut fixé à 5)
+  const [nbItems, setNbItems] = useState(5);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/offers");
+        const response = await axios.get(
+          `http://localhost:3001/offers?page=${page}&limit=${nbItems}`
+        );
         // const response = await axios.get(
         //   "https://vinted-phoenix.herokuapp.com/offers"
         // );
@@ -22,10 +29,9 @@ const HomePage = () => {
 
         // Pagination
         let elem = 1;
-        let limit = 3; // À modifier avec state du <select>
         const newPages = [...pages];
 
-        for (let i = 1; i <= response.data.count; i += limit) {
+        for (let i = 1; i <= response.data.count; i += nbItems) {
           newPages.push(elem);
           elem = elem + 1;
         }
@@ -38,7 +44,22 @@ const HomePage = () => {
       }
     };
     fetchData();
-  }, [setData]);
+  }, [setData, setPages, nbItems]);
+
+  // Fonction de sélection du nombre d'offres/pages
+  const handleNbItems = (e) => {
+    const value = Number(e.target.value);
+    //console.log(value);
+    setNbItems(value);
+    // Ré-initialiser sinon accumulation des tab. de pages
+    setPages([]);
+  };
+
+  // Fonction d'affichage d'une page
+  const handleClickPage = (item) => {
+    console.log("clic sur page ", item);
+    setPage(item);
+  };
 
   return isLoading ? (
     <Loader
@@ -50,16 +71,44 @@ const HomePage = () => {
       timeout={3000} //3 secs
     />
   ) : (
-    <div>
+    <main>
       <Hero />
-      <Offers data={data} />
-      {/* Afficher pages */}
-      <div className="pages-container">
-        {pages.map((item, index) => {
-          return <div key={index}>{item}</div>;
-        })}
+      <div className="container">
+        {/* Selecteur affichage des offres */}
+        <div>
+          <span>Nombre de produits par page :</span>
+          <select
+            value={nbItems}
+            onChange={handleNbItems}
+            name="nbItemsPerPage"
+            id="nbItemsPerPage"
+          >
+            <option value="">--Please choose an option--</option>
+            <option value="5">5 produits par page</option>
+            <option value="10">10 produits par page</option>
+            <option value="15">15 produits par page</option>
+          </select>
+        </div>
+
+        <Offers data={data} />
+
+        {/* Pagination */}
+        <div className="pages-container">
+          {pages.map((item, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => {
+                  handleClickPage(item);
+                }}
+              >
+                {item}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
